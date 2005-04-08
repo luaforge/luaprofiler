970082c2d4b1f5b0dcb8a3220928c7d974197770
Fabio Mascarenhas <mascarenhas@luaforge.net>
2005-04-08 17:21:32 +0000

    Removed some unecessary files

--- src/analyzer/Config.lua
+++
@@ -1,5 +0,0 @@
-
--- the name of the files that represents the graph
--- first  %s: the profiler log file name
--- second %s: the depth-first-listing number of the vertex
-anlC_graph_file_name = "%s_stack_%s.graph"
