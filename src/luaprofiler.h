3f17a6da4840650fd032118cfd554e85852761f9
Fabio Mascarenhas <mascarenhas@luaforge.net>
2005-06-13 19:34:58 +0000

    Added cvs taglines, _VERSION and _COPYRIGHT

--- src/luaprofiler.h
+++ src/luaprofiler.h
@@ -1,6 +1,7 @@
 /*****************************************************************************
 luaProfiler.h:
     Must be included by your main module, in order to profile Lua programs
+** $Id: luaprofiler.h,v 1.2 2005-06-13 19:34:58 mascarenhas Exp $
 *****************************************************************************/
 
 
